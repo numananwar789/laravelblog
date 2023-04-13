d8a5f65563b97364f842ab98af7b7171bc4eb11b
Numan Anwar <numananwar789@gmail.com>
2023-04-13 11:14:01 +0500

    ....

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -69,8 +69,8 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'username' => env('DB_USERNAME', 'user_laravel'),
+            'password' => env('DB_PASSWORD', 'user_laravel123'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
